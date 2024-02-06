cabb9ac72975ffb113d9f597803a111101f7db0f
muhamemed <muhammedtaplamaci@gmail.com>
2024-02-07 00:42:47 +0300

    MakeTakip uygulaması
    mevcut proje için github üzerinden repo oluşturuldu ve dosyalar yüklendi

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager:ISupplierService
+    {
+        private IApplicantService _applicantService; // personmanager i new ile nesne oluşturmak yerine interface ini kullanıyoruz 
+        public PttManager(IApplicantService applicantService) //consturactor
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            PttManager pttManager = new PttManager(new ForeignerManager());   
+           
+            if (_applicantService.CheckPerson(person)) 
+            {
+             Console.WriteLine(person.FirstName + "için maske verildi ");   
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + "için maske Verilemedi ");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,111 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //degiskenler();
+            Vatandas vatandas1 = new Vatandas();
+            vatandas1.Ad="muhammed";
+            SelamVer("muhammed");
+            SelamVer("melih");
+            SelamVer();
+      
+            int sonuc = Topla(6,58);
+            Console.WriteLine(sonuc);
+
+            //Arrays
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "engin";
+            ogrenciler[1] = "kerem";
+            ogrenciler[2] = "berkay";
+            ogrenciler = new string[4];
+            ogrenciler[3] = "ilker";
+
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            string[] sehirler1 = new[] { "ankar", "istanbul", "izmir" };
+            string[] sehirler2 = new[] {"bursa","antalya","diyarbakır" };
+            sehirler2 = sehirler1;
+            sehirler1[0] = "adana";
+            Console.WriteLine("buraaaa "+sehirler2[0]);
+
+
+            foreach (string sehir in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+
+            Person person1 = new Person();
+            person1.FirstName = "MUHAMMED";
+            person1.LastName = "TAPLAMACI";
+            person1.NationalIdentity = 123;
+            person1.DateOfBirthYear = 1985;
+
+
+            List<string> yenisehirler1 = new List<string> { "ankara","istanbul","izmir" };
+            yenisehirler1.Add("Adana 1");
+            Console.WriteLine("----------------");
+            foreach (var sehir in yenisehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+
+        static void SelamVer(string isim="isimsiz")
+        {
+            Console.WriteLine("Merhaba "+ isim);
+        }
+
+        static int Topla(int sayi1=5, int sayi2=10)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam: "+sonuc.ToString());
+            return sonuc;
+        }
+
+        private static void degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000;
+            double oran = 1.18;
+            int sayi = 100;
+            bool isAutonticate = false;
+            string ad = "muhammed";
+            string soyad = "muhammed";
+            int dogumYili = 1996;
+            long tcNo = 12345678910;
+
+            Console.WriteLine(mesaj);
+            Console.WriteLine(mesaj);
+            Console.WriteLine(tutar * oran);
+        }
+    }
+    class Vatandas
+    {
+       
+       
+      
+        public string Ad { get; set; }
+        public string Aoyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
